c828c52d98408df0011c2cdf9b91cc39052951c5
JukeDuke <masterofegy@gmail.com>
2022-06-12 18:19:59 -0400

    new

---
+++ GSCMasterGuide.API/Controllers/PokemonController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace GSCMasterGuide.API.Controllers
+{
+    [Route("/[controller]")]
+    [ApiController]
+    public class PokemonController : ControllerBase
+    {
+    }
+}
