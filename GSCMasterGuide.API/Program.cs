c3d9de086467e2374270295b6ad1c8d002b2a40e
JukeDuke <masterofegy@gmail.com>
2022-06-13 01:55:23 -0400

    Added DbContext

--- GSCMasterGuide.API/Program.cs
+++ GSCMasterGuide.API/Program.cs
@@ -1,6 +1,12 @@
+using GSCMasterGuide.Infrastructure.Data;
+using GSCMasterGuide.Infrastructure.Seeding;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+
+builder.Services.AddDbContext<GSCDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("GSCDb")));
 
 var app = builder.Build();
 
@@ -9,5 +15,11 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<GSCDbContext>();
+    PokemonSeed.Seed(dbContext);
+}
 
 app.Run();
