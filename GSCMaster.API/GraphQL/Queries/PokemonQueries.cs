51b2ce2dfac7cd8482bff9c159f7eb27b7249f87
JukeDuke <masterofegy@gmail.com>
2022-08-24 01:39:17 -0400

    Pokemon query recieves a response instead

--- GSCMaster.API/GraphQL/Queries/PokemonQueries.cs
+++ GSCMaster.API/GraphQL/Queries/PokemonQueries.cs
@@ -1,13 +1,13 @@
 using GSCMaster.Application.CQRS.Pokemon.Queries;
-using GSCMaster.Core.Entities;
+using GSCMaster.Contracts.Responses;
 using MediatR;
 
 namespace GSCMaster.API.GraphQL.Queries;
 public class PokemonQueries
 {
-    public async Task<IReadOnlyCollection<Pokemon>> GetAllPokemon([Service] IMediator mediator, CancellationToken cancellationToken)
+    public async Task<IReadOnlyCollection<PokemonResponse>> GetAllPokemon([Service] IMediator mediator, CancellationToken cancellationToken)
         => await mediator.Send(new GetAllPokemonQuery(), cancellationToken);
 
-    public async Task<Pokemon?> GetPokemonByName([Service] IMediator mediator, string name, CancellationToken cancellationToken)
+    public async Task<PokemonResponse?> GetPokemonByName([Service] IMediator mediator, string name, CancellationToken cancellationToken)
         => await mediator.Send(new GetPokemonQuery(name), cancellationToken);
 }
